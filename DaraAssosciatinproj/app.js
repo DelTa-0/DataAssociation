239a92b5d91c37fdfaff391083f73ed7304f06bf
DelTa-0 <aryalmadhave123@gmail.com>
2025-06-05 17:12:19 +0545

    profile ui done

--- DaraAssosciatinproj/app.js
+++ DaraAssosciatinproj/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 
 app.get('/',(req,res)=>{
-    res.render("index");
+    res.render("profile");
 })
 
 app.post('/register',async(req,res)=>{
@@ -40,10 +40,9 @@ app.post('/register',async(req,res)=>{
     })
 })
 
-app.get('/profile',isLoggedIn,(req,res)=>{
-    console.log(req.user);
-    
-    res.render("login");
+app.get('/profile',isLoggedIn,async(req,res)=>{
+    let user=await userModel.findOne({email:req.user.email});
+    res.render("profile",{user});
 })
 
 app.get('/login',(req,res)=>{
@@ -59,7 +58,7 @@ app.post('/login',async(req,res)=>{
         if(result){ 
             let token=jwt.sign({email,userid:user._id},"secret");
             res.cookie("token",token);
-            res.status(200).send("you can login");
+            res.status(200).redirect("/profile");
         }
         else res.redirect('/login');
     })
@@ -73,7 +72,7 @@ app.get('/logout',(req,res)=>{
 
 function isLoggedIn(req,res,next){
     if(req.cookies.token===""){
-        res.send("need to log in");
+        res.redirect("/login");
     }
     else{
         let data=jwt.verify(req.cookies.token,"secret");
@@ -81,5 +80,7 @@ function isLoggedIn(req,res,next){
         next();
     }
 }
+
+
 
 app.listen(3000);
