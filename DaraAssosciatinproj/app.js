25ee84a9920a351507f9b712a33b2382970ed038
DelTa-0 <aryalmadhave123@gmail.com>
2025-06-05 17:41:22 +0545

    functionality to add post added

--- DaraAssosciatinproj/app.js
+++ DaraAssosciatinproj/app.js
@@ -2,85 +2,123 @@ const express = require('express');
 const ejs = require('ejs');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const userModel = require('./models/user');
 const postModel = require('./models/post');
 
-const cookieParser = require('cookie-parser');
-const app=express();
-app.set('view engine','ejs');
+const app = express();
+app.set('view engine', 'ejs');
 app.use(express.json());
-
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Redirect to login or profile based on token
+app.get('/', (req, res) => {
+    const token = req.cookies.token;
+    if (!token) return res.redirect("/register");
 
-
-app.get('/',(req,res)=>{
-    res.render("profile");
-})
-
-app.post('/register',async(req,res)=>{
-    let {username,name,age,email,password}=req.body;
-    let user=await userModel.findOne({email});
-    if(user) return res.status(500).send("user already registered");
-    bcrypt.genSalt(10,(err,salt)=>{
-        bcrypt.hash(password,salt,async(err,hash)=>{
-        let user=await userModel.create({
-        username,
-        name,
-        age,
-        email,
-        password:hash
-    })
-    let token=jwt.sign({email,userid:user._id},"secret");
-    res.cookie("token",token);
-    res.send("registration done");
-        })
-    })
-})
-
-app.get('/profile',isLoggedIn,async(req,res)=>{
-    let user=await userModel.findOne({email:req.user.email});
-    res.render("profile",{user});
-})
-
-app.get('/login',(req,res)=>{
-    res.render("login");
-})
-
-app.post('/login',async(req,res)=>{
-    let {email,password}=req.body;
-    let user=await userModel.findOne({email});
-    if(!user) return res.status(500).send("Something went wrong");
-    
-    bcrypt.compare(password,user.password,(err,result)=>{
-        if(result){ 
-            let token=jwt.sign({email,userid:user._id},"secret");
-            res.cookie("token",token);
-            res.status(200).redirect("/profile");
-        }
-        else res.redirect('/login');
-    })
-    
-})
-
-app.get('/logout',(req,res)=>{
-    res.cookie("token","");
-    res.redirect("/login");
-})
-
-function isLoggedIn(req,res,next){
-    if(req.cookies.token===""){
+    try {
+        jwt.verify(token, "secret");
+        res.redirect("/profile");
+    } catch (err) {
         res.redirect("/login");
     }
-    else{
-        let data=jwt.verify(req.cookies.token,"secret");
-        req.user=data;
+});
+
+app.get('/register',(req,res)=>{
+    res.render('index')
+})
+
+// Registration route
+app.post('/register', async (req, res) => {
+    try {
+        const { username, name, age, email, password } = req.body;
+        const existingUser = await userModel.findOne({ email });
+        if (existingUser) return res.status(400).send("User already registered");
+
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const user = await userModel.create({
+            username, name, age, email, password: hash
+        });
+
+        const token = jwt.sign({ email, userid: user._id }, "secret");
+        res.cookie("token", token);
+        res.send("Registration done");
+    } catch (err) {
+        res.status(500).send("Registration failed");
+    }
+});
+
+// Protected profile page
+app.get('/profile', isLoggedIn, async (req, res) => {
+    const user = await userModel.findOne({ email: req.user.email }).populate('posts');
+    res.render("profile", { user });
+});
+
+// Create a post
+app.post('/post', isLoggedIn, async (req, res) => {
+    try {
+        const { content } = req.body;
+        const user = await userModel.findOne({ email: req.user.email });
+
+        const post = await postModel.create({
+            user: user._id,
+            content
+        });
+
+        user.posts.push(post._id);
+        await user.save();
+        await user.populate('posts');
+
+        res.redirect("/profile");
+    } catch (err) {
+        res.status(500).send("Failed to create post");
+    }
+});
+
+// Login
+app.get('/login', (req, res) => {
+    res.render("login");
+});
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) return res.status(400).send("Invalid email or password");
+
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+        const token = jwt.sign({ email, userid: user._id }, "secret");
+        res.cookie("token", token);
+        res.redirect("/profile");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+// Logout
+app.get('/logout', (req, res) => {
+    res.cookie("token", "");
+    res.redirect("/login");
+});
+
+// Middleware to check login
+function isLoggedIn(req, res, next) {
+    const token = req.cookies.token;
+    if (!token) return res.redirect("/login");
+
+    try {
+        const data = jwt.verify(token, "secret");
+        req.user = data;
         next();
+    } catch (err) {
+        res.redirect("/login");
     }
 }
 
-
-
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server started on http://localhost:3000");
+});
