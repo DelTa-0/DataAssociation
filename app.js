e333d0aa882c20239025c2977cdfecad400a922a
DelTa-0 <aryalmadhave123@gmail.com>
2025-06-05 14:01:41 +0545

    created user and post

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userModel = require('./models/user');
 const postModel = require('./models/post');
+const post = require('./models/post');
 
 const app=express();
 
@@ -15,6 +16,18 @@ app.get('/create',async(req,res)=>{
         age:18
     })
     res.send(createduser);
+})
+
+//create post
+app.get('/post/create',async(req,res)=>{
+    let post =await postModel.create({
+        postdata:"hello this is an example post",
+        user:"684150fae7ff58a8a91f4445"
+    })
+    let user =await userModel.findOne({_id:"684150fae7ff58a8a91f4445"});
+    user.posts.push(post._id);
+    await user.save();
+    res.send({post,user});
 })
 
 
